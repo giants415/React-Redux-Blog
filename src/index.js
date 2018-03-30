754cf17c1b4082314b94d1970296bc10d982d68a
Andrew <andrew.vinocur@gmail.com>
2018-03-30 11:46:38 -0700

    in using react router, app.js is no longer needed since there isn't a central component anymore

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -13,7 +12,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        
+
       </div>
     </BrowserRouter>
   </Provider>

1387af80a3cb0e73e7cb11b3b3f5f652c1610bbc
Andrew <andrew.vinocur@gmail.com>
2018-03-30 11:45:43 -0700

    cleans out test code

--- src/index.js
+++ src/index.js
@@ -9,21 +9,11 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-  render() {return <div>Hello!</div>}
-}
-
-class Goodbye extends React.Component {
-  render() {return <div>Goodbye!</div>}
-}
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <h1>Welcome to the page!</h1>
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        
       </div>
     </BrowserRouter>
   </Provider>

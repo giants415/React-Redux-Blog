b483436a1407597cdfb9ff3fc177158be0af6075
Andrew <andrew.vinocur@gmail.com>
2018-04-05 15:30:48 -0700

    begins DRYing code

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,7 +3,7 @@ import { Field, reduxForm } from 'redux-form';
 
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
       <div className="form-group">
         <label>Title</label>
@@ -16,20 +16,17 @@ class PostsNew extends Component {
     );
   }
 
-  renderTagsField(field) {
-
-  }
 
   render() {
     return (
       <form>
         <Field
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
         />
         <Field
           name="tags"
-          component={this.renderTagsField}
+          component={this.renderField}
         />
       </form>
     )

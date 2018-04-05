7c36a0fcdf606b93737012ec62c2b7d7be456389
Andrew <andrew.vinocur@gmail.com>
2018-04-05 15:07:07 -0700

    fixes typo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducerm
+  posts: PostsReducer,
   form: formReducer
 });
 

7a0fd98ae57de5a5673fc07835976a76cd83ebc5
Andrew <andrew.vinocur@gmail.com>
2018-04-06 15:25:25 -0700

    adds lifecycle stage to fetch posts

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    this.props.fetchPost();
+  }
+
   render() {
     return(
       <div>

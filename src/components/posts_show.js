0803a006ec94b71ec5f225e36937bd0c6438247f
Andrew <andrew.vinocur@gmail.com>
2018-04-06 16:41:47 -0700

    creates onDeleteClick fn and wires up future deletePost action to connect

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id);
   }
 
   render() {
@@ -37,4 +43,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);

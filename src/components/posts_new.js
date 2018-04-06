301ce7aa8e0066b7a8691a7f783692682c1f1a70
Andrew <andrew.vinocur@gmail.com>
2018-04-06 13:40:34 -0700

    imports

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 
 class PostsNew extends Component {

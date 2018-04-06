1cf1bfb8dba1d087513874b5846dee3a87975973
Andrew <andrew.vinocur@gmail.com>
2018-04-06 13:59:21 -0700

    boiler plate

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return(
+      <div>
+        Posts Show!
+      </div>
+    );
+  };
+}
+
+export default PostsShow;

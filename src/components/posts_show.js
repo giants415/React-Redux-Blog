9a1c1f3f3ad4163eab76b85cbd1d869ae6319389
Andrew <andrew.vinocur@gmail.com>
2018-04-06 16:38:02 -0700

    adds button for deleting post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -19,6 +19,11 @@ class PostsShow extends Component {
     return(
       <div>
         <Link to="/" className="btn btn-primary">Back to Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
